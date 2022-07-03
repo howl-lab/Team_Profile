076019916f54e6ea2149cc9ec5fae709b60cae7f
howl-lab@github.com <howl-lab@github.com>
2022-07-03 00:13:42 -0700

    restructure inquirer prompt logic a bit to split up the main and the additional questions based on role

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 
 // create questions
-const questions = [
+const questionsMain = [
     {
         message: "What's your name?",
-        name: 'name',
+        name: 'managerName',
         type: 'input',
         default: 'nameless worker'
     },
     {
         message: "What's your employee ID?",
-        name: 'employeeID',
+        name: 'managerID',
         type: 'input',
         default: '000-00'
     },
     {
         message: "What's your email?",
-        name: 'email',
+        name: 'managerEmail',
         type: 'input',
         default: "worker@work.com"
     },
@@ -32,41 +32,52 @@ const questions = [
         default: "00"
     },
     {
-        message: "What are you here?",
-        name: 'position',
+        message: "Would you like to add:",
+        name: 'employee',
         type: 'list',
         choices: [
-            'Employee',
-            'Manager'
+            'Engineer',
+            'Intern',
+            'No thanks'
         ]
     },
     {
-        when: (answers) => answers.position === 'Manager',
-        message: "Would you like to:",
-        name: 'actionList',
-        type: 'list',
-        choices: [
-            'add an engineer',
-            'add an intern',
-            'finish up please'
-        ]
-    },
-    {
-        when: (answers) => answers.actionList === 'add an engineer',
+        when: (answers) => answers.employee === 'Engineer',
         message: "What is the GitHub name?",
         name: 'githubName',
         type: 'input',
         default: '@some-github-name'
     },
     {
-        when: (answers) => answers.actionList === 'add an intern',
+        when: (answers) => answers.employee === 'Intern',
         message: "What school do they hail from?",
         name: 'school',
         type: 'input',
         default: 'some school'
     },
+
 ];
 
+const questionsMore = [
+    {
+        message: "What's your employee name?",
+        name: 'employeeName',
+        type: 'input',
+        default: 'nameless worker'
+    },
+    {
+        message: "What's your employee ID?",
+        name: 'employeeID',
+        type: 'input',
+        default: '000-00'
+    },
+    {
+        message: "What's your employee email?",
+        name: 'employeeEmail',
+        type: 'input',
+        default: "worker@work.com"
+    },
+]
 
 // inquirer to ask the questions
 function init() {
@@ -81,10 +92,10 @@ function init() {
 
 
 // create write to file for each person group
-// const jess = new Manager('Jess', 1, '123-123-1234');
-// fs.writeFile('dist/team.html', jess.generateCard(), (err) => {
-//     if (err) throw err;
-// });
+const jess = new Manager('Jess', 1, '123-123-1234');
+fs.writeFile('dist/team.html', jess.generateCard(), (err) => {
+    if (err) throw err;
+});
 
 // Function call to initialize app
 init();
